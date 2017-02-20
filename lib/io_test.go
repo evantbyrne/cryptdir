27c69f85739f4941a95b35d79d03f503a571896a
Evan Byrne <evantbyrne@gmail.com>
2017-02-20 15:18:36 -0500

    Test EncryptFile and DecryptFile

--- lib/io_test.go
+++ lib/io_test.go
@@ -1,14 +1,49 @@
 package lib
 
 import (
+	"io/ioutil"
+	"log"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
 
+func TestEncryptFileAndDecryptFile(t *testing.T) {
+	var (
+		cleartext1 []byte
+		cleartext2 []byte
+		key1       []byte
+		key2       []byte
+		nonce1     []byte
+		nonce2     []byte
+		password   []byte
+		salt1      []byte
+		salt2      []byte
+	)
+
+	tmp, err := ioutil.TempFile("", "")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	cleartext1 = []byte("The quick brown fox jumps over the lazy dog.")
+	password = []byte("mysuperdupersecurepassword")
+	salt1 = NewNonce()
+	key1 = NewKey(salt1, password)
+	nonce1 = NewNonce()
+
+	EncryptFile(tmp.Name(), cleartext1, nonce1, key1, salt1)
+	cleartext2, nonce2, key2, salt2 = DecryptFile(tmp.Name(), password)
+
+	assert.Equal(t, cleartext1, cleartext2, "cleartext should match")
+	assert.Equal(t, nonce1, nonce2, "nonce should match")
+	assert.Equal(t, key1, key2, "key should match")
+	assert.Equal(t, salt1, salt2, "salt should match")
+}
+
 func TestGetRawFileName(t *testing.T) {
 	var (
-		config Config
+		config     Config
 		filenameA1 string
 		filenameA2 string
 		filenameB1 string
